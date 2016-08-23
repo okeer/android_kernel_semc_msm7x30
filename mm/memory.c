9276f8215fee97fdcfcd330ef0f5a5d797f6c12e
Okeer <serhio.amadey@gmail.com>
2016-08-23 11:32:05 +0700

    Squashed update of kernel from 3.4.107 to 3.4.112
    This squashed update doesn't include the following commits:
    	de2a293... Bluetooth: ath3k: Add support of AR3012 bluetooth 13d3:3423 device
    	a30355f... ath9k: declare required extra tx headroom
    	1660790... ath5k: fix spontaneus AR5312 freezes
    	c5cb7dd... ath9k: fix DMA stop sequence for AR9003+
    	6f82bf6... xhci: Workaround for PME stuck issues in Intel xhci
    	b111371... Bluetooth: ath3k: Add support Atheros AR5B195 combo Mini PCIe card
    	df48a54... mmc: core: add missing pm event in mmc_pm_notify to fix hib restore
    	f3f4500... ath3k: add support of 13d3:3474 AR3012 device
    
    These commits are merged earlier:
    	7cddc7d... selinux/nlmsg: add XFRM_MSG_GETSPDINFO
    	ac18a53... selinux/nlmsg: add XFRM_MSG_[NEW|GET]SADINFO
    	0fcabf0... selinux/nlmsg: add XFRM_MSG_REPORT
    	fa0ed22... selinux/nlmsg: add XFRM_MSG_MIGRATE
    	6ebf033... selinux/nlmsg: add XFRM_MSG_MAPPING
    	39af20b... vfs: read file_handle only once in handle_to_path
    	842c362... md: use kzalloc() when bitmap is disabled
    	7014d74... ipv4: Missing sk_nulls_node_init() in ping_unhash()
    	a39bf4a... pipe: iovec: Fix memory corruption when retrying atomic copy as non-atomic
    	1c50a0a... udp: fix behavior of wrong checksums
    	c4b0cf5... mmc: card: Fixup request missing in mmc_blk_issue_rw_rq
    	39f7979... net: add validation for the socket syscall protocol argument
    	48ec02c... KEYS: Fix race between key destruction and finding a keyring by name
    	9793b7b... KEYS: Fix crash when attempt to garbage collect an uninstantiated keyring
    	59c8392... ipv6: addrconf: validate new MTU before applying it
    	b2e3078... ARM: 7880/1: Clear the IT state independent of the Thumb-2 mode
    	b381fbc... pipe: Fix buffer offset after partially failed read
    	e661bb1... fs: take i_mutex during prepare_binprm for set[ug]id executables
    
    Change-Id: I12ad90f09fe045895e8af6ef7a34e231c9bae971

--- mm/memory.c
+++ mm/memory.c
@@ -3211,6 +3211,10 @@ static int do_anonymous_page(struct mm_struct *mm, struct vm_area_struct *vma,
 	if (vma->vm_flags & VM_SHARED)
 		return VM_FAULT_SIGBUS;
 
+	/* File mapping without ->vm_ops ? */
+	if (vma->vm_flags & VM_SHARED)
+		return VM_FAULT_SIGBUS;
+
 	/* Check if we need to add a guard page to the stack */
 	if (check_stack_guard_page(vma, address) < 0)
 		return VM_FAULT_SIGSEGV;
@@ -3536,7 +3540,6 @@ int handle_pte_fault(struct mm_struct *mm,
 			if (vma->vm_ops)
 				return do_linear_fault(mm, vma, address,
 						pte, pmd, flags, entry);
-
 			return do_anonymous_page(mm, vma, address,
 					pte, pmd, flags);
 		}
@@ -3887,7 +3890,7 @@ int generic_access_phys(struct vm_area_struct *vma, unsigned long addr,
 	if (follow_phys(vma, addr, write, &prot, &phys_addr))
 		return -EINVAL;
 
-	maddr = ioremap_prot(phys_addr, PAGE_SIZE, prot);
+	maddr = ioremap_prot(phys_addr, PAGE_ALIGN(len + offset), prot);
 	if (write)
 		memcpy_toio(maddr + offset, buf, len);
 	else
